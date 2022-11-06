5487fcb9e79c0c27746c0f929ae9464f2d9cfd17
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-11-06 19:50:47 +0300

    sumeyye

--- src/test/java/get/Get07b.java
+++ src/test/java/get/Get07b.java
@@ -12,27 +12,6 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class Get07b extends GoRestBaseUrl {
-    /*
-        Given
-            https://gorest.co.in/public/v1/users/2986
-        When
-            User send GET Request to the URL
-        Then
-            Status Code should be 200
-        And
-            Response body should be like
-            /*
-      {
-    "meta": null,
-    "data": {
-        "id": 2986,
-        "name": "Brijesh Kocchar",
-        "email": "kocchar_brijesh@prosacco.com",
-        "gender": "female",
-        "status": "active"
-    }
-}
-     */
 
     @Test
     public void test01() {
