b327520596faffaa886a40f87f16911c6f5a3aa3
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-11-12 12:39:58 +0300

    sumeyye

--- src/test/java/post_requests/Post05ObjectMapper_Pojo.java
+++ src/test/java/post_requests/Post05ObjectMapper_Pojo.java
@@ -1,4 +1,56 @@
 package post_requests;
 
-public class Post05ObjectMapper_Pojo {
+import baseURL.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+import utils.ObjectMapperUtils;
+
+import java.awt.geom.RectangularShape;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Post05ObjectMapper_Pojo extends JsonplaceholderBaseUrl {
+     /*
+         Given
+           1) https://jsonplaceholder.typicode.com/todos
+           2) {
+                 "userId": 55,
+                 "title": "Tidy your room",
+                 "completed": false
+               }
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like  {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+
+    @Test
+    public void pojo01() {
+        //set the url
+        spec.pathParam("first","todos");
+
+        //set the expected data
+        JsonPlaceHolderPojo expectedData=new JsonPlaceHolderPojo(55,"Tidy your room",false);
+        System.out.println("expectedData = " + expectedData);
+        //send the request get the response
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).post("/{first}");
+        response.prettyPrint();
+
+        //do assertion
+        JsonPlaceHolderPojo actualData=ObjectMapperUtils.convertJsonToJava(response.asString(),JsonPlaceHolderPojo.class);
+        System.out.println("actualData = " + actualData);
+        assertEquals(201,response.statusCode());
+        assertEquals(expectedData.getUserId(),actualData.getUserId());
+        assertEquals(expectedData.getCompleted(),actualData.getCompleted());
+        assertEquals(expectedData.getTitle(),actualData.getTitle());
+    }
 }
